5ebe8306b7760d8f32bdc5eeee19a8d46eb340ea
Vision-Paudel <visionpaudel@gmail.com>
2023-06-18 18:27:33 -0400

    Settings Activity and Changes related to it.

--- app/src/main/java/com/example/a670groupproject/SettingsActivity.java
+++ app/src/main/java/com/example/a670groupproject/SettingsActivity.java
@@ -3,12 +3,19 @@ package com.example.a670groupproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.Switch;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    Switch musicSwitch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        musicSwitch = findViewById(R.id.musicSwitch);
+
     }
 }
